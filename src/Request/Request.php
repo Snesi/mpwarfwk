cbb1c7b922b2395db522b03a6d69d72bfa55d6d0
David G. Shannon <gooster.snesi@gmail.com>
2015-06-28 20:46:48 +0200

    Added code to Request class to process the http parameters

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -1,9 +1,45 @@
 <?php
 namespace MPWAR\Request;
 
-class Request {
+class Request
+{
+	const URI_OFFSET = 1;
+	const REQUEST_METHOD = "REQUEST_METHOD";
+	const REQUEST_URI = "REQUEST_URI";
+    
+    private $get;
+    private $post;
+    private $server;
+    private $cookies;
+    private $uri;
+    private $session;
+    private $method;
+    private $uri_components;
 
-	public function __construct() {
-		
-	}
+    
+    public function __construct() {
+        $this->createParameters();
+        $this->processUri();
+    }
+    
+    private function createParameters() {
+        $this->get = new Parameters($_GET);
+        $this->post = new Parameters($_POST);
+        $this->server = new Parameters($_SERVER);
+        $this->cookies = new Parameters($_COOKIE);
+        
+        $this->method = $_SERVER[self::REQUEST_METHOD];
+        $this->uri = $_SERVER[self::REQUEST_URI];
+    }
+
+    private function processUri() {
+    	$this->uri_components = array_slice(explode("/", $this->uri), self::URI_OFFSET);
+    }
+
+    public function __get($property) {
+    	if(isset($this->$property)){
+    		return $this->$property;
+    	}
+    	return null;
+    }
 }
