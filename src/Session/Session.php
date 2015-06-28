df19a81e59ef1e4dfd77741467fda90f48b27a44
David G. Shannon <gooster.snesi@gmail.com>
2015-06-28 20:45:00 +0200

    Added Session class

---
+++ src/Session/Session.php
@@ -0,0 +1,21 @@
+<?php
+namespace MPWAR\Session;
+
+class Session
+{
+    
+    public function __construct() {
+        session_start();
+    }
+    
+    public function __get($key) {
+        if(isset($_SESSION[$key])) {
+            return $_SESSION[$key];
+        }
+        return null;
+    }
+
+    public function __set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+}
