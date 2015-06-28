695c2c51130a4fefb517079f09222edd2c496077
David G. Shannon <gooster.snesi@gmail.com>
2015-06-28 20:44:28 +0200

    Added Parameter bag class

---
+++ src/Request/Parameters.php
@@ -0,0 +1,18 @@
+<?php
+namespace MPWAR\Request;
+class Parameters
+{
+    private $params;
+    public function __construct($params) {
+        $this->params = $params;
+    }
+    public function __get($key) {
+        if (isset($this->params[$key])) {
+            return $this->params[$key];
+        }
+        return null;
+    }
+    public function __set($key, $value) {
+        $this->params[$key] = $value;
+    }
+}
