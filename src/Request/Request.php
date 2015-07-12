42c76c47629bf1a716e2920f7cdbb0f8c29b143b
David G. Shannon <gooster.snesi@gmail.com>
2015-07-12 18:53:19 +0200

    Added json check to see if the user is requesting a json response

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -7,7 +7,9 @@ class Request
     const REQUEST_METHOD = "REQUEST_METHOD";
     const REQUEST_URI = "REQUEST_URI";
     const QUERY_START = "?";
-    
+    const HTTP_ACCEPT = "HTTP_ACCEPT";
+    const JSON_CONTENT = "application/json";
+
     private $get;
     private $post;
     private $server;
@@ -17,19 +19,19 @@ class Request
     private $method;
     private $uri_components;
 
-    
+
     public function __construct()
     {
         $this->createParameters();
     }
-    
+
     private function createParameters()
     {
         $this->get = new Parameters($_GET);
         $this->post = new Parameters($_POST);
         $this->server = new Parameters($_SERVER);
         $this->cookies = new Parameters($_COOKIE);
-        
+
         $this->method = $_SERVER[self::REQUEST_METHOD];
         $this->uri = $this->processUri($_SERVER[self::REQUEST_URI]);
     }
@@ -37,6 +39,11 @@ class Request
     private function processUri($request_uri)
     {
         return strtok($request_uri, self::QUERY_START);
+    }
+
+    public function isJson()
+    {
+        return $this->server->HTTP_ACCEPT === self::JSON_CONTENT;
     }
 
     public function __get($property)
